e8cae765d77c4113e8a3e1873ca8fba2d33246de
StrangeDuckk <mikusek.kasia@gmail.com>
2025-03-22 14:31:37 +0100

    added empty file

---
+++ Cw-2-s30338/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw_2_s30338
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello world");
+        }
+    }
+}

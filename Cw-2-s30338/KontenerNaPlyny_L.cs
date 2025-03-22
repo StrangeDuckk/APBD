467f42e3f6dea14e8dc43308d8c6c82c3708000b
StrangeDuckk <mikusek.kasia@gmail.com>
2025-03-22 16:03:04 +0100

    separeted classes into seprate files

---
+++ Cw-2-s30338/KontenerNaPlyny_L.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw_2_s30338
+{
+    internal class Class1
+    {
+    }
+}

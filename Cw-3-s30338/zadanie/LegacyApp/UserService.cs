1d0bcb50948f39cd4e232d002b83f42a7fe84640
StrangeDuckk <mikusek.kasia@gmail.com>
2025-03-30 19:59:17 +0200

    initial commit

---
+++ Cw-3-s30338/zadanie/LegacyApp/UserService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    //TO ZMIENIAMY
+    //ma dzialac tak jak dzialalo przed refaktoryzacja, ma dzialac dokladnie tak samo jak dzialala przed nami
+    //testy -> zrefaktoryzowana ta metoda, bo bedziemy musieli pisac testy sprawdzajace czy refaktoryzacja sie powiodla
+    public class UserService
+    {
+        //tylko i wylacznie add user mozemy dodac i usuwac
+        //nie mozemy nic usunac, mozemy cos dodac w innych kladach
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                return false;
+            }
+
+            if (!email.Contains("@") && !email.Contains("."))
+            {
+                return false;
+            }
+
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (client.Type == "ImportantClient")
+            {
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+            }
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+    }
+}

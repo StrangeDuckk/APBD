816ad116d9ace075c2db728bbc08a270f590effb
StrangeDuckk <mikusek.kasia@gmail.com>
2025-04-05 16:51:47 +0200

    finished 1-10 tests

--- Cw-4-s30338/Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Cw-4-s30338/Tutorial4/Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -146,8 +146,12 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        // var result = null; 
-        //
-        // Assert.Contains("ALLEN", result);
+        var result = emps.Where(e => e.Sal > 
+                                            emps.Where(s => s.DeptNo == e.DeptNo)//subquery
+                                                .Average(s => s.Sal))
+                         .Select(e => e.EName)
+                         .ToList(); 
+        
+        Assert.Contains("ALLEN", result);
     }
 }

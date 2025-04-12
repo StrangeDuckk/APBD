0dacdb22538ea52a881998459ea01206679f6eda
StrangeDuckk <mikusek.kasia@gmail.com>
2025-04-12 16:18:39 +0200

    added zwierze model and controller for it

---
+++ Cw-5-s30338/WebApplication1/WebApplication1/Models/Zwierze.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Zwierze
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Kategoria { get; set; }
+        public double Masa { get; set; }
+        public string KolorSiersci { get; set; }
+    }
+}

a10a887b18a243e57ae4d4fe87dddd81d18a762b
StrangeDuckk <mikusek.kasia@gmail.com>
2025-06-05 16:37:54 +0200

    classes with ef database first

---
+++ Cw-10-s30338/Cw-10-s30338/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cw_10_s30338.Models;
+
+public partial class Country
+{
+    public int IdCountry { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Trip> IdTrips { get; set; } = new List<Trip>();
+}
